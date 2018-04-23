934d920316c36133acf0f7c8d1891395c6b3aa63
Alewi Ahmed <alewimail@gmail.com>
2018-04-23 16:45:52 +0300

    Show x when a user clicks a single cell.

--- src/App.js
+++ src/App.js
@@ -4,26 +4,49 @@ import './App.css';
 import SingleCell from './components/SingleCell/';
 
 class App extends Component {
+  state = {
+    cells: ['', '', '', '', '', '', '', '', ''],
+    player1: 'X',
+    currentTurn: 'player1',
+    multiplayer: false
+  };
+
+  selectCell = id => {
+    this.setState(state => {
+      state.cells[id] = state[state.currentTurn];
+      return state;
+    });
+  };
+
+  showCells = () => {
+    let { cells } = this.state;
+    let cellsElement = [];
+    let innerCells = [];
+    cells.forEach((_, index) => {
+      innerCells.push(
+        <SingleCell
+          id={index}
+          key={index}
+          value={cells[index]}
+          onClick={this.selectCell}
+        />
+      );
+      if ((index + 1) % 3 == 0) {
+        cellsElement.push(
+          <div className="row" key={index + 1 / 3}>
+            {innerCells}
+          </div>
+        );
+        innerCells = [];
+      }
+    });
+    return cellsElement;
+  };
+
   render() {
     return (
       <div className="App">
-        <div className="container">
-          <div className="row">
-            <SingleCell id="1" value="" borderRight borderBottom />
-            <SingleCell id="2" value="" borderRight borderBottom />
-            <SingleCell id="3" value="" borderBottom />
-          </div>
-          <div className="row">
-            <SingleCell id="4" value="" borderRight borderBottom />
-            <SingleCell id="5" value="" borderRight borderBottom />
-            <SingleCell id="6" value="" borderBottom />
-          </div>
-          <div className="row">
-            <SingleCell id="7" value="" borderRight />
-            <SingleCell id="8" value="" borderRight />
-            <SingleCell id="9" value="" />
-          </div>
-        </div>
+        <div className="container">{this.showCells()}</div>
       </div>
     );
   }
